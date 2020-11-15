47816c821f16ef5e2ea679c4b1440333b64dfe6f
marco <mana-99@outlook.com>
2020-11-15 19:15:39 +0100

    new downloader

---
+++ docs/twitter-api.md
@@ -0,0 +1,46 @@
+
+## Search
+
+Standard Search: This search API searches against a sampling of recent Tweets published 
+in the past 7 days. Part of the 'public' set of APIs. [^1]
+
+
+Resource URL: https://api.twitter.com/1.1/search/tweets.json
+
+Resource Information 
+
+* Response formats	JSON
+* Requires authentication?	Yes
+* Rate limited?	Yes
+* Requests / 15-min window (user auth)	180
+* Requests / 15-min window (app auth)	450
+
+## Parameters
+
+https://developer.twitter.com/en/docs/twitter-api/v1/tweets/search/api-reference/get-search-tweets
+
+
+### Query parameter
+
+https://developer.twitter.com/en/docs/twitter-api/v1/tweets/search/guides/standard-operators
+
+| Operator | Finds Tweets... |
+| --- | --- |
+| puppy filter:media | containing “puppy” and an image or video. |
+| puppy \-filter:retweets | containing “puppy”, filtering out retweets |
+| puppy filter:native\_video | containing “puppy” and an uploaded video, Amplify video, Periscope, or Vine. |
+| puppy filter:periscope | containing “puppy” and a Periscope video URL. |
+| puppy filter:vine | containing “puppy” and a Vine. |
+| puppy filter:images | containing “puppy” and links identified as photos, including third parties such as Instagram. |
+| puppy filter:twimg | containing “puppy” and a pic.twitter.com link representing one or more photos. |
+| superhero since:2015\-12\-21 | containing “superhero” and sent since date “2015\-12\-21” (year\-month\-day). |
+| puppy until:2015\-12\-21 | containing “puppy” and sent before the date “2015\-12\-21”. |
+| movie \-scary :) | containing “movie”, but not “scary”, and with a positive attitude. |
+| flight :( | containing “flight” and with a negative attitude. |
+| traffic ? | containing “traffic” and asking a question. |
+
+
+[^1]: https://developer.twitter.com/en/docs/twitter-api/v1/tweets/search/overview
+
+
+
--- downloader/app/download_tweets.py
+++ downloader/app/download_tweets.py
@@ -1,98 +1,130 @@
 
-import concurrent.futures
-import pymongo
-from pymongo.write_concern import WriteConcern
-import tweepy
-import configparser
 import logging
-import logging.config
-#import numpy as np
-from os.path import dirname, abspath, join
+import pymongo
+import tweepy
+import time
 
-CURRENT_DIR = dirname(abspath(__file__))
-INI_PATH = join(CURRENT_DIR, '.ini')
-LOGGER_PATH = join(CURRENT_DIR, 'logger.conf')
-print(INI_PATH)
-print(LOGGER_PATH)
+from dotenv import load_dotenv
+from os import environ
+from pymongo.write_concern import WriteConcern
 
-conf = configparser.ConfigParser()
-conf.read(INI_PATH)
-
-logging.config.fileConfig(fname=LOGGER_PATH)
-logger = logging.getLogger("downloader")
-
-# twitter authentication
-auth = tweepy.OAuthHandler(conf['twitter']['consumer_key'], conf['twitter']['consumer_key_secret'])
-auth.set_access_token(conf['twitter']['access_token'], conf['twitter']['access_token_secret'])
-api = tweepy.API(auth, wait_on_rate_limit=True)
-
-# mongo authentication
-#conn_str = f"mongodb://{conf['mongo']['user']}:{conf['mongo']['pass']}@{conf['mongo']['host']}"
-conn_str = "mongodb://{}:{}@{}".format(conf['mongo']['user'],
-                                       conf['mongo']['pass'],
-                                       conf['mongo']['host'])
-db = pymongo.MongoClient(conn_str)[conf['mongo']['db']]
-coll = db.get_collection(conf['mongo']['collection'])
+MAX_REQUESTS = 180
+TIME_WINDOW_MINS = 15
+DB_NAME = 'twitter'
+COLLECTION_NAME = 'salvo-matteini-bot'
 
 
-class mresults:
-    SKIPPED  = 1, 0, 0, 0
-    UPDATED  = 0, 1, 0, 0
-    INSERTED = 0, 0, 1, 0
-    FAILED   = 0, 0, 0, 1
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+load_dotenv()
 
 
-def upsert_single(tweet_dict):
+def init() -> (tweepy.API, pymongo.collection.Collection):
+
+    # twitter authentication
+    auth = tweepy.OAuthHandler(environ['TWITTER_CONSUMER_KEY'], environ['TWITTER_CONSUMER_KEY_SECRET'])
+    auth.set_access_token(environ['TWITTER_ACCESS_TOKEN'], environ['TWITTER_ACCESS_TOKEN_SECRET'])
+    api = tweepy.API(auth)  #, wait_on_rate_limit=True)   # wait on rate limit when sending requests
+
+    logger.debug(f"API version: {api.api_root[1:]}")
+    logger.debug(f"Cache: {api.cache}")
+    logger.debug(f"Timeout: {api.timeout}")
+
+    # mongo authentication
+    coll = pymongo.MongoClient(environ['MONGO_CONNECTION_STRING'])[DB_NAME].get_collection(COLLECTION_NAME)
+
+    return api, coll
+
+
+def upsert_single(tweet: dict, coll: pymongo.collection.Collection):
+
+    class mresults:
+        SKIPPED  = 1, 0, 0, 0
+        UPDATED  = 0, 1, 0, 0
+        INSERTED = 0, 0, 1, 0
+        FAILED   = 0, 0, 0, 1
 
     coll_w = coll.with_options(write_concern=WriteConcern(w='majority'))
     try:
-        upsert_result = coll_w.update_one(filter={'id': tweet_dict['id']},
-                                          update={'$set': tweet_dict},
+        upsert_result = coll_w.update_one(filter={'id': tweet['id']},
+                                          update={'$set': tweet},
                                           upsert=True)
         r = upsert_result.raw_result
         if r['n'] == 1 and r['ok'] == 1:
             if r['updatedExisting']:
                 if r['nModified'] == 1:
-                    logger.debug(f"{tweet_dict['id_str']} - Updated")
+                    logger.debug(f"{tweet['id_str']} - Updated")
                     return mresults.UPDATED
                 else:
-                    logger.debug(f"{tweet_dict['id_str']} - Skipped")
+                    logger.debug(f"{tweet['id_str']} - Skipped")
                     return mresults.SKIPPED
             elif r['upserted']:
-                logger.debug(f"{tweet_dict['id_str']} - Inserted")
+                logger.debug(f"{tweet['id_str']} - Inserted")
                 return mresults.INSERTED
-        logger.error(f"{tweet_dict['id_str']} - Failed")
+        logger.error(f"{tweet['id_str']} - Failed")
         return mresults.FAILED
     except OverflowError as e:
-        logger.error(f"{tweet_dict['id_str']} - OverflowError ({e})")
+        logger.error(f"{tweet['id_str']} - OverflowError ({e})")
         return mresults.FAILED
     except pymongo.errors.ServerSelectionTimeoutError as e:
-        logger.error(f"{tweet_dict['id_str']} - ServerSelectionTimeoutError ({e})")
+        logger.error(f"{tweet['id_str']} - ServerSelectionTimeoutError ({e})")
         return mresults.FAILED
 
 
-words = 'salvini -filter:retweets'
-date_from = "2020-01-01"
-max_tweets = 100
+def scan(api: tweepy.API, since_id: int = 0):
 
-tweets = tweepy.Cursor(api.search,
-                       q=words, lang="it", since=date_from, tweet_mode='extended').items(max_tweets)
+    logger.debug("Twitter API search request")
 
-MAX_WORKERS = 5
+    return tweepy.Cursor(api.search,
+                         # q="salvini -filter:retweets",
+                         q="salvini -filter:media -filter:retweets -filter:native_video -filter:periscope "
+                           "-filter:vine -filter:images -filter:twimg",
+                         result_type='recent',
+                         lang="it",
+                         since_id=since_id,
+                         # count=100,
+                         # since=date_from,
+                         tweet_mode='extended')
 
-try:
-    with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
-       map_out = executor.map(upsert_single, (t._json for t in tweets))
-except tweepy.error.RateLimitError as e:
-    logger.error(e)
+
+def main():
+
+    api, coll = init()
+    started_at = time.time()
+    max_id = next(coll.aggregate([
+        {'$group': {'_id': None, 'max_id': {'$max': '$id'}}}
+    ]))['max_id']
+
+    # max requests event
+    def on_max_requests():
+        nonlocal started_at
+        wait_seconds = TIME_WINDOW_MINS * 60 - (time.time() - started_at)
+        logger.warning(f"Too many requests. Waiting {int(wait_seconds/60)} minutes...")
+        time.sleep(wait_seconds)
+        started_at = time.time()
+
+    # main loop
+    while True:
+        tweets = scan(api, since_id=max_id)
+        try:
+            for i, t in enumerate(tweets.items()):
+                if t.id > max_id:
+                    max_id = t.id
+                res = upsert_single(t._json, coll)
+                logger.debug(f"{t.id_str} - {res}")
+        except tweepy.error.RateLimitError:
+            on_max_requests()
+        except tweepy.error.TweepError:
+            on_max_requests()
+
+main()
 
 
-from functools import reduce
+# import concurrent.futures
+# from functools import reduce
+# with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+#     map_out = executor.map(upsert_single, (t._json for t in tweets), (coll for t in tweets))
+#     skipped, updated, inserted, failed = reduce(lambda x, y: (x[0]+y[0], x[1]+y[1], x[2]+y[2], x[3]+y[3]),
+#                                                 list(map_out))
+#     logger.info(f"skipped: {skipped}, updated: {updated}, inserted: {inserted}, failed: {failed}")
 
-skipped, updated, inserted, failed = reduce(lambda x, y: (x[0]+y[0],
-                                                          x[1]+y[1],
-                                                          x[2]+y[2],
-                                                          x[3]+y[3]), list(map_out))
-# summary = np.array(list(map_out)).sum(axis=0)
-# skipped, updated, inserted, failed = tuple(summary)
-logger.info(f"skipped: {skipped}, updated: {updated}, inserted: {inserted}, failed: {failed}")
